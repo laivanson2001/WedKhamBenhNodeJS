04339967bb346bb3a225fb96c55902831a47a076
Son Lai <laivanson.pc@gmail.com>
2022-06-10 01:54:43 +0700

    upload

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize');
 
 
 // Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize('test', 'root', null, {
+const sequelize = new Sequelize('wedkhambenh', 'root', null, {
     host: 'localhost',
     dialect: 'mysql',
     logging: false,
