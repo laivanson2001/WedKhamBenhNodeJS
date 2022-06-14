9d82310f7e0aa67074bf323272666c317c184b4e
Son Lai <laivanson.pc@gmail.com>
2022-06-14 23:54:07 +0700

    #36

--- src/server.js
+++ src/server.js
@@ -3,9 +3,18 @@ import bodyParser from "body-parser"
 import viewEngine from "./config/viewEngine"
 import initWedRoutes from "./route/wed"
 import connectDB from "./config/connectDB";
+
 require('dotenv').config()
 let app = express()
 
+const cors = require('cors');
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', "http://localhost:3000");
+    res.setHeader('Access-Control-Allow-Headers', "*");
+    res.header('Access-Control-Allow-Credentials', true);
+    next();
+});
+app.use(cors({ origin: true }));
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
